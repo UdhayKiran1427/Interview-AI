701a29abad79b2f9f47c05c0f3dc730be1ebd72d
UdhayKiran1427 <udhaykiranande@gmail.com>
2026-05-27 18:29:49 +0530

    space

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const connectToDB = require("./src/config/database")
 connectToDB()
 
 
+
 app.listen(process.env.PORT || 3000,'0.0.0.0', () => {
     console.log("Server is running on port 3000")
 })
