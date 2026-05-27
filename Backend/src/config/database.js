50c6af1db581bdacdfea23bbc4f22449711d9027
UdhayKiran1427 <udhaykiranande@gmail.com>
2026-05-27 17:21:10 +0530

    changed to mongodb atlas

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,4 +1,10 @@
+const dns = require("dns")
 const mongoose = require("mongoose")
+
+// Use public DNS servers for SRV resolution when local DNS refuses the query.
+// This helps with mongodb+srv Atlas URIs on some Windows/network environments.
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+
 
 
 
