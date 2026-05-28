2a0ecd8e288f9655d556c2c4114e08398d7a49bc
UdhayKiran1427 <udhaykiranande@gmail.com>
2026-05-28 12:00:50 +0530

    cors error fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,11 +3,21 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 
 const app = express()
-
+const allowedOrigins = ["https://interview-ai-5gy9-udhaykiran1427s-projects.vercel.app","https://interview-ai-5gy9.vercel.app/login"]
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://interview-ai-5gy9-udhaykiran1427s-projects.vercel.app",
+    origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl, or Postman)
+    if (!origin) return callback(null, true);
+    
+    // Check if the origin is in our allowed list, or use a regex to match all Vercel previews
+    if (allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.vercel.app')) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
